54f7fa6ab5d5da6e87ba140ece9c27551617fecd
michaelujkim <michaelujkim@gmail.com>
2018-04-05 13:51:41 -0700

    error handling

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -25,8 +25,8 @@ def get_filters():
 
     # get user input for day of week (all, monday, tuesday, ... sunday)
     city = input("Enter city: ")
-    month = input("Enter month: ")
-    day = input("Enter day: ")
+    month = input("Enter month as an integer (0=all,1=January,etc.): ")
+    day = input("Enter day as integer(0=all, 1=monday, etc.): ")
 
     return city, month, day
 
@@ -42,28 +42,46 @@ def load_data(city, month, day):
     Returns:
         df - Pandas DataFrame containing city data filtered by month and day
     """
-    raw_csv = CITY_DATA[city]
-    raw_df = pd.read_csv(raw_csv)
-    raw_df['Start Time'] = pd.to_datetime(raw_df['Start Time'])
-    raw_df['month'] = raw_df['Start Time'].dt.month
 
-    raw_df['day'] = raw_df['Start Time'].dt.weekday
-    raw_df['hour'] = raw_df['Start Time'].dt.hour
-    month_int = int(month)
-    day_int = int(day)
-    if month_int == 0:
-        month_sorted_df = raw_df
+    if city not in CITY_DATA:
+        print("City is not in list")
+        df = pd.DataFrame()
+    elif city == '':
+        print("Please enter city")
+        df = pd.DataFrame()
+    elif month == '':
+        print("Please enter month")
+        df = pd.DataFrame()
+    elif day == '':
+        print("Please enter day")
+        df = pd.DataFrame()
     else:
-        month_df = raw_df['month'] == month_int
-        month_sorted_df = raw_df[month_df]
-    if day_int == 0:
-        df = raw_df
-    else:
+        raw_csv = CITY_DATA[city]
+        raw_df = pd.read_csv(raw_csv)
+        raw_df['Start Time'] = pd.to_datetime(raw_df['Start Time'])
+        raw_df['month'] = raw_df['Start Time'].dt.month
 
-        day_df = month_sorted_df['day'] == day_int
-        df = month_sorted_df[day_df]
+        raw_df['day'] = raw_df['Start Time'].dt.weekday
+        raw_df['hour'] = raw_df['Start Time'].dt.hour
+        month_int = int(month)
+        day_int = int(day)
+        if month_int == 0:
+            month_sorted_df = raw_df
+        elif month_int > 12:
+            print("Not a valid month")
+            df = pd.DataFrame()
+        else:
+            month_df = raw_df['month'] == month_int
+            month_sorted_df = raw_df[month_df]
+        if day_int == 0:
+            df = raw_df
+        elif day_int > 7:
+            print("Not a valid day")
+            df = pd.DataFrame()
+        else:
 
-    print(df)
+            day_df = month_sorted_df['day'] == day_int
+            df = month_sorted_df[day_df]
     return df
 
 
@@ -72,12 +90,15 @@ def time_stats(df):
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
+    if df.empty:
+        print("Something went wrong")
 
-    print(df['month'].value_counts().idxmax())
+    else:
+        print(df['month'].value_counts().idxmax())
 
-    print(df['day'].value_counts().idxmax())
+        print(df['day'].value_counts().idxmax())
 
-    print(df['hour'].value_counts().idxmax())
+        print(df['hour'].value_counts().idxmax())
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
@@ -88,13 +109,16 @@ def station_stats(df):
 
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
+    if df.empty:
+        print("Something went wrong")
+    else:
 
-    print(df['Start Station'].value_counts().idxmax())
+        print(df['Start Station'].value_counts().idxmax())
 
-    print(df['End Station'].value_counts().idxmax())
+        print(df['End Station'].value_counts().idxmax())
 
-    print(df.groupby(['Start Station', 'End Station']
-                     ).size().idxmax())
+        print(df.groupby(['Start Station', 'End Station']
+                         ).size().idxmax())
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
@@ -105,10 +129,12 @@ def trip_duration_stats(df):
 
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
+    if df.empty:
+        print("Something went wrong")
+    else:
+        print(df['Trip Duration'].sum())
 
-    print(df['Trip Duration'].sum())
-
-    print(df['Trip Duration'].mean())
+        print(df['Trip Duration'].mean())
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
@@ -119,15 +145,15 @@ def user_stats(df):
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
+    if df.empty:
+        print("Something went wrong")
+    else:
+        print(df['User Type'].value_counts().idxmax())
 
-    print(df['User Type'].value_counts().idxmax())
-
-    print(df['Gender'].value_counts().idxmax())
-
-    # Display earliest, most recent, and most common year of birth
-    print(df['Birth Year'].max())
-    print(df['Birth Year'].min())
-    print(df['Birth Year'].value_counts().idxmax())
+        print(df['Gender'].value_counts().idxmax())
+        print(df['Birth Year'].max())
+        print(df['Birth Year'].min())
+        print(df['Birth Year'].value_counts().idxmax())
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
